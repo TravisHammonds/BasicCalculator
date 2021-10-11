dc9a975aab6d31a7b91e3b2bf28ff7e18b5a3015
Travis Hammonds <travishammonds2@gmail.com>
2021-10-10 21:31:39 -0400

    initialization

---
+++ script.js
@@ -0,0 +1,47 @@
+/* 
+Travis Hammonds 
+CalculatorApp 
+v 1.o
+Sep 9, 2021
+*/
+ class Calculator {
+     constructor (previousOperand, currentOperand) {
+         this.previousOperand = previousOperand
+         this.currentOperand = currentOperand
+     }
+ }
+
+ clear(); {
+
+ }
+
+ deleteNum(); {
+
+ }
+
+ 
+appendNumber(); {
+
+}
+
+ chooseOperation(); {
+
+ }
+
+equals(); {
+
+}
+
+
+
+const numberButtons = document.querySelectorAll('{data-number');
+const operationButtons = document.querySelectorAll('{data-operation');
+const deleteButton = document.querySelectorAll('{data-delete');
+const allClearButton = document.querySelectorAll('{data-ac');
+const equalsButton = document.querySelectorAll('{data-equals');
+const previousOperand = document.querySelectorAll('{data-previous-op');
+const currentOperand = document.querySelectorAll('{data-current-op');
+
+
+
+
